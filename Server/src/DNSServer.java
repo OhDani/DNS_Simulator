45e9d703ce0cc42f0ed3cff98cd1e63cb4a9f7db
OhDani <tranngoc2k41511@gmail.com>
2024-09-21 16:00:18 +0700

    [Update] IP query from simple domain name + cache

--- Server/src/DNSServer.java
+++ Server/src/DNSServer.java
@@ -6,22 +6,19 @@ import java.net.*;
 
 class DNSServer {
 	public static void main(String[] args) throws Exception {
-		//Create a socket object called sSock, of type ServerSocket for TCP.
 		ServerSocket sSock = null;
-		int idNumber = 1;	// keeping track of users since multi users can do queries to server
+		int idNumber = 1;
 		try {
-			// Try to open server socket 5001.
 			sSock =  new ServerSocket(5001);
 		} catch (Exception e) {
 			System.out.println("Could not listen on port: 5001.");
-			System.exit(1); // Handle exceptions.
+			System.exit(1);
 		}
 
 		System.out.println("Server is listening...");
-		new monitorQuit().start(); // Start a new thread to monitor exit signal.
+		new monitorQuit().start();
 
 		while (true) {
-			//If there is a pending client connection, start new query thread.
 			new dnsQuery(sSock.accept(), idNumber).start();
 			System.out.printf(">> User %d connected:\n", idNumber);
 			idNumber++;
